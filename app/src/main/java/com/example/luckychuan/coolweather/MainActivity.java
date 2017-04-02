47353db34edd5bf3d235d038abde5d37fe4962f8
Luckychuan <luckychuan96@163.com>
2017-04-02 22:25:27 +0800

    完成初始化工作

--- app/src/main/java/com/example/luckychuan/coolweather/MainActivity.java
+++ app/src/main/java/com/example/luckychuan/coolweather/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String URL = "http://guolin.tech/api/weather?cityid=CN101110101&key=386188613f8e460388361160675a0a82";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
