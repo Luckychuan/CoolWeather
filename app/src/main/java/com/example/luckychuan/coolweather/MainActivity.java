01a96ac2d529ff0874bac60beed2281a9ea8781d
Luckychuan <luckychuan96@163.com>
2017-04-03 21:28:58 +0800

    使用okhttp下载数据

--- app/src/main/java/com/example/luckychuan/coolweather/MainActivity.java
+++ app/src/main/java/com/example/luckychuan/coolweather/MainActivity.java
@@ -1,16 +1,60 @@
 package com.example.luckychuan.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String URL = "http://guolin.tech/api/weather?cityid=CN101110101&key=386188613f8e460388361160675a0a82";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        downloadData(URL);
+
     }
+
+    private void downloadData(String url){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+
+           client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "获取信息失败！", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse( Response response) throws IOException {
+                    final String result = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d(TAG, "onCreate: " + result);
+                        }
+                    });
+                }
+            });
+
+    }
+
+
+
 }
